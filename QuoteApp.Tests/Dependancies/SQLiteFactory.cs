a0077793ddee44b49aca7373227795cb1593043c
rid00z <rid00z@gmail.com>
2014-06-18 09:35:36 +1000

    add contact page model tests

---
+++ QuoteApp.Tests/Dependancies/SQLiteFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using QuoteApp.Dependancies;
+using SQLite.Net.Platform.Generic;
+
+namespace QuoteApp.Tests.Dependancies
+{
+    public class SQLiteFactory : ISQLiteFactory
+    {
+        public SQLite.Net.SQLiteConnection CreateConnection (string dbName)
+        {
+            return new SQLite.Net.SQLiteConnection (new SQLitePlatformGeneric (), "test.db");
+        }
+
+        public SQLiteFactory ()
+        {
+        }
+    }
+}
+
---
+++ QuoteApp.Tests/PageModels/ContactPageModelTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using QuoteApp.PageModels;
+using QuoteApp.Services;
+using QuoteApp.Tests.Dependancies;
+using QuoteApp.Models;
+using FakeItEasy;
+using QuoteApp.Pages;
+
+namespace QuoteApp.Tests.PageModels
+{
+    [TestFixture]
+    public class ContactPageModelTests
+    {
+        [Test]
+        public static void CreateNewContact()
+        {
+            var container = A.Fake<IRootNavigation> ();
+            TinyIoC.TinyIoCContainer.Current.Register<IRootNavigation> (container);
+
+            var db = new DatabaseService (new SQLiteFactory());
+            var vm = new ContactPageModel (db);
+            vm.Init (null);
+
+            //save to database
+            vm.Contact.Name = "Peter";
+            vm.Contact.Phone = "9087";
+            vm.Done.Execute (null);
+
+            Assert.IsTrue (vm.Contact.ContactId > 0);
+
+            //get from database
+            var savedContact 
+                    = db.Conn.Table<Contact> ().Where ((c) => c.ContactId == vm.Contact.ContactId).FirstOrDefault ();
+
+            Assert.AreEqual ("Peter", savedContact.Name);
+            Assert.AreEqual ("9087", savedContact.Phone);
+            A.CallTo (() => container.PopPage ()).MustHaveHappened ();
+        }
+    }
+}
+
